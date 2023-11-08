7b01025d6c1b586a18ded1ac9e70debe17e0d6ad
Mikuller <fasikamillion75@gmail.com>
2023-11-08 10:41:12 -0900

    applied todo update functionaity

--- app/Livewire/TodoList.php
+++ app/Livewire/TodoList.php
@@ -14,6 +14,11 @@ class TodoList extends Component
     public $name;
 
     public $search;
+
+    public $edittingTodoId;
+
+    #[Rule('required|min:1|max:300')]
+    public $edittingTodoName;
     public function create()
     {
         $validated = $this->validateOnly('name');
@@ -23,18 +28,43 @@ class TodoList extends Component
         $this->reset('name');
 
         session()->flash('success', 'created');
+
+        $this->resetPage();
     }
-    public function delete(Todo $todo){
+    public function delete(Todo $todo)
+    {
         $todo->delete();
     }
 
-    public function toggle(Todo $todo){
-      $todo->completed = !$todo->completed;
-      $todo->save();
+    public function edit($todoId)
+    {
+        $this->edittingTodoId = $todoId;
+        $this->edittingTodoName = Todo::find($todoId)->name;
+    }
+    public function update(Todo $todo)
+    {
+        $validated = $this->validateOnly('edittingTodoName');
+        $todo->update([
+            'name'=> $this->edittingTodoName,
+        ]);
+        $this->reset('edittingTodoName', 'edittingTodoId');
+
+    }
+    public function cancelEdit()
+    {
+        $this->reset('edittingTodoName', 'edittingTodoId');
+    }
+
+    public function toggle(Todo $todo)
+    {
+        $todo->completed = !$todo->completed;
+        $todo->save();
     }
     public function render()
     {
-        $toDoList = Todo::latest()->where('name','like',"%{$this->search}%")->paginate(3);
+        $toDoList = Todo::latest()
+            ->where('name', 'like', "%{$this->search}%")
+            ->paginate(3);
         return view('livewire.todo-list', compact('toDoList'));
     }
 }
--- resources/views/livewire/includes/show_todo_list.blade.php
+++ resources/views/livewire/includes/show_todo_list.blade.php
@@ -3,23 +3,31 @@
     <div class="flex justify-between space-x-2">
 
         <div class="flex">
-            <!-- <input type="text" placeholder="Todo.."
-                    class="bg-gray-100  text-gray-900 text-sm rounded block w-full p-2.5"
-                    value="Todo Name">
-                
-                    <span class="text-red-500 text-xs block">error</span> -->
-            @if ($toDo->completed )
+
+            @if ($toDo->completed)
                 <input wire:click="toggle({{ $toDo->id }})" type="checkbox" class="mr-2" checked>
             @else
                 <input wire:click="toggle({{ $toDo->id }})" type="checkbox" class="mr-2">
             @endif
 
-            <h3 class="text-lg text-semibold text-gray-800">{{ $toDo->name }}</h3>
+            @if ($edittingTodoId == $toDo->id)
+            <div>
+                <input wire:model="edittingTodoName" type="text" placeholder="Todo.." name="todoName"
+                    class="mt-2 bg-gray-100  text-gray-900 text-sm rounded block w-full p-2.5">
+                @error('edittingTodoName')
+                    <span class="text-red-500 text-xs block">{{ $message }}</span>
+                @enderror
+            </div>
+            @else
+                <h3 class="text-lg text-semibold text-gray-800">{{ $toDo->name }}</h3>
+            @endif
+
         </div>
 
 
         <div class="flex items-center space-x-2">
-            <button class="text-sm text-teal-500 font-semibold rounded hover:text-teal-800">
+            <button wire:click="edit({{ $toDo->id }})"
+                class="text-sm text-teal-500 font-semibold rounded hover:text-teal-800">
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
                     stroke="currentColor" class="w-4 h-4">
                     <path stroke-linecap="round" stroke-linejoin="round"
@@ -38,11 +46,10 @@
     </div>
     <span class="text-xs text-gray-500"> {{ $toDo->created_at }} </span>
     <div class="mt-3 text-xs text-gray-700">
-        <!--
-                <button
-                    class="mt-3 px-4 py-2 bg-teal-500 text-white font-semibold rounded hover:bg-teal-600">Update</button>
-                <button
-                    class="mt-3 px-4 py-2 bg-red-500 text-white font-semibold rounded hover:bg-red-600">Cancel</button> -->
-
+        @if ($edittingTodoId == $toDo->id)
+            <button wire:click="update({{$toDo->id}})"
+                class="mt-3 px-4 py-2 bg-teal-500 text-white font-semibold rounded hover:bg-teal-600">Update</button>
+            <button wire:click="cancelEdit" class="mt-3 px-4 py-2 bg-red-500 text-white font-semibold rounded hover:bg-red-600">Cancel</button>
+        @endif
     </div>
 </div>
